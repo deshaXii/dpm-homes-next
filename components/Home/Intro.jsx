951e563050106192f1b07794c971c59dab8d26ed
deshaXii <mustafa.fathi7@gmail.com>
2022-08-16 14:13:21 +0200

    change intro background

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -136,7 +136,7 @@ const HomeIntro = () => {
     >
       <div
         className="home-header-bg"
-        style={{ backgroundImage: "url(/img/home-header-bg.jpg)" }}
+        style={{ backgroundImage: "url(/img/background-cover.jpg)" }}
       >
         &nbsp;
       </div>
