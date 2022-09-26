d4ce12b783f499515ce47bf22bc345743760aa39
deshaXii <mustafa.fathi7@gmail.com>
2022-09-26 18:24:20 +0200

    fix cli issues

--- components/Home/Services.jsx
+++ components/Home/Services.jsx
@@ -27,7 +27,7 @@ const Services = ({ sectionBG, withOverlay, data }) => {
           {services.map((item, index) => (
             <div
               className="col-12 col-md-4"
-              key={item.id}
+              key={index}
               style={{
                 marginTop:
                   index + 1 === 1
--- pages/_document.js
+++ pages/_document.js
@@ -17,8 +17,8 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
             integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
+            crossOrigin="anonymous"
+            `referrerPolicy`="no-referrer"
           />
           <link rel="icon" href="/img/favicon.ico" />
         </Head>
