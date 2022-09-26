493a74d9fb3f87c7b86b78a76f715037635919b1
deshaXii <mustafa.fathi7@gmail.com>
2022-09-26 18:40:21 +0200

    l

--- pages/_document.js
+++ pages/_document.js
@@ -16,9 +16,6 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
           />
           <link rel="icon" href="/img/favicon.ico" />
         </Head>
