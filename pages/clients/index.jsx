66abbc9a76d24971906c7a9bb296afd96a6ff51a
deshaXii <mustafa.fathi7@gmail.com>
2022-10-17 13:18:56 +0200

    add clients page

---
+++ pages/clients/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Default from "../../layouts/default";
+
+const Clients = () => {
+  return (
+    <Default>
+      <div className="clients-page" style={{ padding: "60px 0 120px 0" }}>
+        <div className="container">clients page</div>
+      </div>
+    </Default>
+  );
+};
+
+export default Clients;
